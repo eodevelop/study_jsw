c08b28453b08a69c924b5a2a448027de37328655
eodevelop <eodevelop@naver.com>
2024-10-23 23:07:33 +0900

    프로젝트 생성

---
+++ 스프링 시큐리티 완전 정복 [6.x 개정판]/spring_security_master/src/main/groovy/io/security/spring_security_master/SpringSecurityMasterApplication.groovy
@@ -0,0 +1,13 @@
+package io.security.spring_security_master
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringSecurityMasterApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringSecurityMasterApplication, args)
+    }
+
+}
